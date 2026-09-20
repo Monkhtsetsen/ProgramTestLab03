9bee4d9af85cd15ed4c74f26b43d2120676e949a
Monkhtsetsen <Monkhtsetsen15@gmail.com>
2026-09-20 11:00:17 +0800

    4 dehi nemelt scenerio nemsen

--- slo-test.js
+++ slo-test.js
@@ -4,9 +4,10 @@ export const options = {
   vus: 20,
   duration: "1m",
   thresholds: {
-    "http_req_duration{name:cart}": ["p(95)<200"], // Performance SLO
+    "http_req_duration{name:cart}": ["p(95)<10"], // Performance SLO
     "http_req_failed{name:pay}": ["rate<0.08"], // Reliability SLO
     checks: ["rate>0.90"], // Availability SLO
+    "http_req_duration{name:report}": ["p(95)<450"],
   },
 };
 export default function () {
